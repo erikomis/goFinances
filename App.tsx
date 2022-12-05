4e2ffa405ce9b9d8ac377789e9bbd5e620a4079b
erikMartins <erik.ozzetti.martins@gmail.com>
2022-12-05 03:12:28 -0300

    feat: navigation

--- App.tsx
+++ App.tsx
@@ -10,16 +10,14 @@ import {
 } from "@expo-google-fonts/poppins";
 
 import theme from "./src/global/styles/theme";
-import { Dashboard } from "./src/screens/Dashboard";
-
+import { NavigationContainer } from "@react-navigation/native";
+import { AppRoutes } from "./src/routes/app.routes";
 export default function App() {
-
   const [isLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
     Poppins_700Bold,
   });
-
 
   useEffect(() => {
     const showSplashScreen = async () => {
@@ -38,10 +36,12 @@ export default function App() {
   }, [isLoaded]);
 
   if (!isLoaded) return null;
-  
+
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
