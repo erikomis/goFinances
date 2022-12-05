68c9c49e0fe270c7efa7b475bb1b45a126b2e144
erikMartins <erik.ozzetti.martins@gmail.com>
2022-12-05 03:14:48 -0300

    feat: screen  Register and style

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+import { Modal, TouchableWithoutFeedback, Keyboard, Alert } from "react-native";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import { Button } from "../../components/Form/Button";
+
+import { InputForm } from "../../components/Form/InputForm";
+import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
+import { CategorySelectButton } from "../../components/Form/CategorySelectButton";
+
+import { CategorySelect } from "../CategorySelect";
+
+import {
+  Container,
+  Header,
+  Title,
+  Form,
+  Fields,
+  TransactionTypes,
+} from "./styles";
+
+interface FormData {
+  [name: string]: string;
+}
+
+const schema = Yup.object().shape({
+  name: Yup.string().required("Nome é obrigatório"),
+  amount: Yup.number()
+    .typeError("Informe um valor numérico")
+    .positive("O valor não pode ser negativo")
+    .required("O valor é obrigatório"),
+});
+
+export const Register = () => {
+  const [transactionType, setTransactionType] = useState("");
+  const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+
+  const [category, setCategory] = useState({
+    key: "category",
+    name: "Categoria",
+  });
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
+  const handleTransactionTypeSelect = (type: "up" | "down") => {
+    setTransactionType(type);
+  };
+
+  const handleCloseSelectCategoryModal = () => {
+    setCategoryModalOpen(false);
+  };
+
+  const handleOpenSelectCategoryModal = () => {
+    setCategoryModalOpen(true);
+  };
+
+  const handleRegister = (form: FormData) => {
+    if (!transactionType) return Alert.alert("Selecione o tipo da transação");
+
+    if (category.key === "category")
+      return Alert.alert("Selecione a categoria");
+
+    const data = {
+      name: form.name,
+      amount: form.amount,
+      transactionType,
+      category: category.key,
+    };
+    console.log(data);
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <Container>
+        <Header>
+          <Title>Cadastro</Title>
+        </Header>
+        <Form>
+          <Fields>
+            <InputForm
+              error={errors.name && errors.name.message}
+              name="name"
+              control={control}
+              placeholder="Nome"
+              autoCapitalize="sentences"
+              autoCorrect={false}
+            />
+            <InputForm
+              error={errors.amount && errors.amount.message}
+              name="amount"
+              control={control}
+              placeholder="Preço"
+              autoCapitalize="sentences"
+              keyboardType="numeric"
+            />
+            <TransactionTypes>
+              <TransactionTypeButton
+                type="up"
+                title="Income"
+                onPress={() => handleTransactionTypeSelect("up")}
+                isActive={transactionType === "up"}
+              />
+              <TransactionTypeButton
+                type="down"
+                title="Outcome"
+                onPress={() => handleTransactionTypeSelect("down")}
+                isActive={transactionType === "down"}
+              />
+            </TransactionTypes>
+            <CategorySelectButton
+              title={category.name}
+              onPress={handleOpenSelectCategoryModal}
+            />
+          </Fields>
+
+          <Button title="Enviar" onPress={handleSubmit(handleRegister)} />
+        </Form>
+        <Modal visible={categoryModalOpen}>
+          <CategorySelect
+            category={category}
+            setCategory={setCategory}
+            closeSelectCategory={handleCloseSelectCategoryModal}
+          />
+        </Modal>
+      </Container>
+    </TouchableWithoutFeedback>
+  );
+};
