edb94470c45d75aae72506f761595b99c88f9f6d
erikMartins <erik.ozzetti.martins@gmail.com>
2022-12-05 21:03:56 -0300

    refator: component

--- src/components/TransactionCard/index.tsx
+++ src/components/TransactionCard/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { categories } from "../../utils/categories";
 import {
   Container,
   Title,
@@ -17,31 +17,34 @@ interface Category {
 }
 
 export interface Props {
-  data: TransactionCardProps
+  data: TransactionCardProps;
 }
 
-export interface TransactionCardProps  {
+export interface TransactionCardProps {
   type: "positive" | "negative";
-  title: string;
+  name: string;
   amount: string;
-  category: Category;
+  category: string;
   date: string;
 }
 
-export const TransactionCard = ({
- data
-}: Props) => {
+export const TransactionCard = ({ data }: Props) => {
+ const [category] = categories.filter(
+    (item) => item.key === data.category
+  );
+
   return (
     <Container>
-      <Title>{data.title}</Title>
+      <Title>{data.name}</Title>
 
-      <Amount type={data.type}>{
-        data.type === "negative" && "- "} {data.amount}</Amount>
+      <Amount type={data.type}>
+        {data.type === "negative" && "- "} {data.amount}
+      </Amount>
 
       <Footer>
         <Category>
-          <Icon name={data.category.icon} />
-          <CategoryName>{data.category.name}</CategoryName>
+          <Icon name={category.icon} />
+          <CategoryName>{category.name}</CategoryName>
         </Category>
         <Date>{data.date}</Date>
       </Footer>
